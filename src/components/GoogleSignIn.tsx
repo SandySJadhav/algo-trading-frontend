8d9fa4a4c8bb49c8523dd78e5ec2011fca12b549
Sandip Jadhav <sandip.jadhav@kohler.com>
2024-01-01 14:14:48 +0530

    fix prettier issue

--- src/components/GoogleSignIn.tsx
+++ src/components/GoogleSignIn.tsx
@@ -1,26 +1,28 @@
-'use client';
-import Firebase from '@services/GoogleApp';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+"use client";
+
+import Button from "./Button";
+import Firebase from "@services/GoogleApp";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { FcGoogle } from "react-icons/fc";
-import Button from './Button';
 
 type Prop = {
-  variant?: string,
-  label?: string
-}
+  variant?: string;
+  label?: string;
+};
 
 const GoogleSignIn = ({ variant, label }: Prop) => {
   const handleOnClick = () => {
     signInWithPopup(Firebase.auth, Firebase.provider)
       .then((result) => {
-        console.log(result)
+        console.log(result);
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential?.accessToken;
         // The signed-in user info.
         const user = result.user;
         console.log(user);
-      }).catch((error) => {
+      })
+      .catch((error) => {
         console.log(error);
         // Handle Errors here.
         const errorCode = error.code;
@@ -30,18 +32,20 @@ const GoogleSignIn = ({ variant, label }: Prop) => {
         // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error);
       });
-  }
+  };
 
-  return <Button
-    type='button'
-    variant={variant}
-    onClick={handleOnClick}
-    name="google"
-    icon={<FcGoogle />}
-    className="flex justify-center"
-  >
-    {label}
-  </Button>
-}
+  return (
+    <Button
+      type="button"
+      variant={variant}
+      onClick={handleOnClick}
+      name="google"
+      icon={<FcGoogle />}
+      className="flex justify-center"
+    >
+      {label}
+    </Button>
+  );
+};
 
 export default GoogleSignIn;
--- src/components/StoreWrapper.tsx
+++ src/components/StoreWrapper.tsx
@@ -1,12 +1,13 @@
-'use client'
-import { ReactNode } from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-import store from '../redux/store';
+"use client";
+
+import store from "../redux/store";
+import { ReactNode } from "react";
+import { Provider as ReduxProvider } from "react-redux";
 
 type Prop = { children: ReactNode };
 
 const StoreWrapper = ({ children }: Prop) => {
-    return <ReduxProvider store={store}>{children}</ReduxProvider>
-}
+  return <ReduxProvider store={store}>{children}</ReduxProvider>;
+};
 
 export default StoreWrapper;
